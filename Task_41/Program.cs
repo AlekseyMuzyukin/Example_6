c803fac37870c5b6e596514d5f231bbe70f45c3c
Aleksey <J2k_pot@mail.ru>
2022-10-05 13:29:45 +0300

    add 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите числа (через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+int PositiveNum(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+
+    }
+
+    return count;
+
+}
+
+int result = PositiveNum(array);
+
+Console.WriteLine($"Количество элементов больше нуля -> {result}");
